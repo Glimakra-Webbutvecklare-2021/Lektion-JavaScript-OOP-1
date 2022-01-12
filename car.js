e637b8c22261ea6756a3aa8adc790477151d2262
unknown <anders.sjunnesson@gmail.com>
2022-01-12 15:29:19 +0100

    Log file functionality

--- car.js
+++ car.js
@@ -3,11 +3,13 @@ class Car {
     constructor(type, color) {
         this.type = type;
         this.color = color;
+        this.batteryCapacity = 100;
+        this.logFile = [];
     }
 
     // en funktion som beskriver aktuell bil
     describe() {
-        console.log(`En ${this.color} bil, med bilmärket ${this.type}`);
+        console.log(`En ${this.color} bil, med bilmärket ${this.type}. Bilens batterivärde är: ${this.batteryCapacity}`);
     }
 
     // en funktion som ändrar en egenskap
@@ -19,12 +21,38 @@ class Car {
         if (colors.includes(color)) {
             this.color = color;
         }
+
+        let history = "Bilen ändrade färg till " + this.color;
+        this.logFile.push(history);
     }
 
     //  en funktion som hämtar en egenskap
     getColor() {
 
         return this.color;
+    }
+
+    // funktioner för att köra omkring...
+    start() {
+
+        // att starta innebär att batteriet förbrukas ngt...
+        this.batteryCapacity -= 1;
+    }
+
+    accelerate() {
+
+        // slumpa en siffra som anger hur mycket batteri går åt...
+        let number = Math.random() * 10;
+
+        // avrunda talet till ett heltal
+        number = Math.floor(number);
+
+        // minska batteriet med motsvarande siffra
+        this.batteryCapacity -= number;
+    }
+
+    turnLeft() {
+
     }
 }
 
@@ -40,3 +68,11 @@ let color = car1.getColor();
 
 // visa bilens färg
 console.log("Bilens färg är nu " + color);
+
+// händelser för en bil
+car1.start();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.describe();
