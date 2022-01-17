3ae8e32f8bb273c37f25cfbadc9d1852d14e8904
unknown <anders.sjunnesson@gmail.com>
2022-01-17 14:29:40 +0100

    Documentation

--- dice.js
+++ dice.js
@@ -4,26 +4,35 @@
 // elementet som visar täningen
 const dice = document.getElementById("dice");
 
+// Promise baserad funktion för att kasta tärning
 function rollDice(numberOfMilliSeconds) {
 
+    // ett promise objekt
     return new Promise((resolve, reject) => {
 
+        // ange default - i det här läget ett resultat som inte är möjligt (en nolla); 
         let diceNumber = 0;
 
         // använd setInterval()
         let intervalId = setInterval(() => {
 
+            // slumpa fram ett tal
             diceNumber = Math.floor(Math.random() * 6 + 1);
 
+            // visa värdet i elementet 
             dice.innerText = diceNumber;
 
         }, 10);
 
         // avbryt tärningskastet efter ett antal millisekunder
         setTimeout(() => {
+
+            // använd metoden clearInterval() och ange id
             clearInterval(intervalId);
 
+            // infria löftet om tärningskastet - resolve
             resolve(diceNumber);
+
         }, numberOfMilliSeconds);
 
     });
@@ -32,14 +41,13 @@ function rollDice(numberOfMilliSeconds) {
 // kasta tärningen i ett slumpat antal millisekunder
 
 let ms = Math.floor(Math.random() * 1000);
-
 console.log(`Tärningen rullar i ${ms} millisekunder`);
 
 rollDice(ms).then((message) => {
     console.log("Resultatet blev: " + message);
 });
 
-
+// Ett promise objekt om ett löfte kan infrias direkt:
 Promise.resolve("Lektionen är till ända....").then((message) => {
     console.log(message);
 });
