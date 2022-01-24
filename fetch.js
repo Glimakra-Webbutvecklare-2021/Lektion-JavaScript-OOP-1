2190f22f754be0dd74ab25f3987d9f8d6ee534bb
unknown <anders.sjunnesson@gmail.com>
2022-01-24 13:12:34 +0100

    Test API cats...

--- fetch.js
+++ fetch.js
@@ -1,15 +1,25 @@
 // använd fetch för att hämta data
 // fetch är promise baserat: hanteras med .then() syntax
 
-let url = "movies.txt";
+let url = "movies.json";
+url = "https://cat-fact.herokuapp.com/facts";
 
 fetch(url)
     .then(function(response) {
 
-        if (!response.ok) {
-            console.log("Ett fel uppstod för att nå källan");
-        }
+        // undersök hur responsen ser ut...
         console.log(response);
+
+        // använd egenskapen ok för response: true | false
+        if (!response.ok) {
+            console.log("Följande fel uppstod: ", response.status);
+        }
+        
+        // använd metoden json() för att returnera innehållet som json objekt
+        return response.json();
+    })
+    .then(function(data) {
+        console.log("Innehållet i filen:", data);
     })
     .catch(function(error) {
         console.log("An error:", error)
