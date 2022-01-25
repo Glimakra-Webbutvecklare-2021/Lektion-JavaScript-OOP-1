aa9c7585dd42446e9b06985b14f7817cf23d7ee8
unknown <anders.sjunnesson@gmail.com>
2022-01-25 06:54:25 +0100

    Catch error

--- fetch.js
+++ fetch.js
@@ -2,7 +2,8 @@
 // fetch är promise baserat: hanteras med .then() syntax
 
 let url = "movies.json";
-url = "https://cat-fact.herokuapp.com/facts";
+url = "https://cat-fact.herokuapp.com/fact";
+url = "https://api.github.com/users/{username}";
 
 fetch(url)
     .then(function(response) {
@@ -13,6 +14,9 @@ fetch(url)
         // använd egenskapen ok för response: true | false
         if (!response.ok) {
             console.log("Följande fel uppstod: ", response.status);
+
+            // aktivera Error genom att "kasta" ett fel, då aktiveras catch() nedan
+            throw new Error(`Error reading resource. Status: ${response.status}`);
         }
         
         // använd metoden json() för att returnera innehållet som json objekt
@@ -20,9 +24,48 @@ fetch(url)
     })
     .then(function(data) {
         console.log("Innehållet i filen:", data);
+
+        
+        // sidan om fakta kring katter...
+        // https://alexwohlbruck.github.io/cat-facts/
+        // data finns i form av en array av 5 objekt
+        // iteration av array
+        // data.forEach(element => {
+            
+            // här är nu "element" det samma som ett objekt
+            // testa att console logga...
+            // console.log(element);
+
+            // eftersom en egenskap är "text", välj ut endast den - punktnotation
+            // console.log(element.text);
+
+            // visa innehållet på webbsidan - ex blockquote
+            // skapa html element
+            // let blockquote = document.createElement("blockquote");
+
+            // ge blockelementet innehållet från aktuellt objekt
+            // blockquote.textContent = element.text;
+
+            // lägg till på sidan
+            // document.getElementById("result").appendChild(blockquote);
+
+        // });
+
+
+        // information från en användare på Github
+        // visa avatar med egenskapen avatar_url
+        // skapa en img element
+        let img = new Image();
+
+        // lägg till källan med info från Github response
+        img.src = data.avatar_url;
+
+        // lägg till sidans innehåll
+        document.getElementById("result").appendChild(img);
+
     })
     .catch(function(error) {
-        console.log("An error:", error)
+        console.log("An error:", error);
     })
     .finally(function() {
         console.log("Den här delen körs alltid");
