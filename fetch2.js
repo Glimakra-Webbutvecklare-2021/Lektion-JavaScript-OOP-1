155c93d885f438271e9bd832ccfb726287664ec4
unknown <anders.sjunnesson@gmail.com>
2022-01-26 11:23:34 +0100

    Fetch example...

--- fetch2.js
+++ fetch2.js
@@ -6,9 +6,9 @@ url = "https://api.github.com/users/andsju";
 
 // Fetch() i JavaScript är promisebaserat - returnerar resultat efter tid
 // hanteras därför med .then() .catch() .finally()
-fetch(url).then(function(response) {
+fetch(url).then(function (response) {
     // console.log(response);
-    
+
     console.log("Response1: ", response);
     // console.log("Response2: " +  response);
     // console.log(`Response3: ${response}`);
@@ -19,14 +19,14 @@ fetch(url).then(function(response) {
     // för att se innehållet så testa först metoden json()
     // console.log(response.json());
     // för att hantera innehållet returnera detsamma, och använd .then()
-    
+
     // metoden .json() använder JSON.parse och presenterar resultatet som ett objekt efter .then() 
 
     // jfr return response.text(); och return response.json();
 
     return response.json();
 
-}).then(function(data) {
+}).then(function (data) {
     console.log("data", data);
 
     // om man använder metoden .text() ovan, kan man konvertera till json med JSON.parse...
@@ -36,13 +36,20 @@ fetch(url).then(function(response) {
     // console.log(obj);
 
     // presentera data på webbsidan
-    result.innerHTML = render(data.company); 
+    result.innerHTML = render(data.company);
 
+}).catch(function (error) {
+
+    // ett fel som man har skapat med throw new Error() i steg ovanför kan hanteras här 
+    // hanteras? ex loggas i en fil, visa ngt vänligt felmeddelande på webbsida 
+
+}).finally(function () {
+
+    // metoden .finally() kan användas för att visa ett innehåll efter det att metoderna ovan är avklarade
 
 });
 
 // en funktion som renderar med innerHTML
-
 function render(something) {
     return `
         <p>
@@ -52,3 +59,41 @@ function render(something) {
     `;
 }
 
+
+// fetch() med function() {}
+fetch(url)
+    .then(function(response) {
+
+        // returnera
+        return response.json(); 
+    })
+    .then(function(obj) {
+
+        // gör ngt med objektet
+        // analysera hur objektet ser ut
+        // visa på sidan kanske...
+
+    });
+
+// fetch med arrow function () => {}
+fetch(url)
+    .then((response) => response.json())
+    // .then((obj) => renderContent(obj));
+    .then((obj) => result.innerHTML = renderContentFromInnerHTML(obj));
+
+function renderContent(obj) {
+
+    // skapa element med document.createElement()
+    let p = document.createElement("p");
+    
+    // lägg till textinnehåll 
+    p.textContent = obj.name;
+
+    // visa på sidan
+    result.appendChild(p);
+}
+
+function renderContentFromInnerHTML(obj) {
+
+    return `<p>${obj.name}</p>`;
+}
